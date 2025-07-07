3868416a813a0eedd7d0c919a9cb824445ef7283
hamedmiri <hamedmiri72@gmail.com>
2025-07-07 10:57:19 +0100

    configuration and eureka service

---
+++ services/discovery/src/test/java/com/hamedTech/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamedTech.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
